50f225d193cc57943ea86cfdbc10447f6ab0ca2f
Oguz Kagan EREN <oguz.eren@ceng.deu.edu.tr>
2020-01-29 16:05:07 +0300

    change mvc version, added list currency

---
+++ CurrencyWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using CurrencyWebApplication.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CurrencyWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            using (var webClient = new System.Net.WebClient())
+            {
+                webClient.Encoding = Encoding.UTF8;
+                var rates = webClient.DownloadString("https://api.exchangeratesapi.io/latest?base=TRY");
+                ExchangeModel exchangeData = JsonConvert.DeserializeObject<ExchangeModel>(rates);
+                ViewBag.exchangeData = exchangeData;
+                var currencies = webClient.DownloadString("https://openexchangerates.org/api/currencies.json");
+                CurrencyModel currencyData = JsonConvert.DeserializeObject<CurrencyModel>(currencies);
+                ViewBag.currencyData = currencyData;
+                
+            }
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Result()
+        {
+            var code = Request.QueryString["value"];
+            var webClient = new System.Net.WebClient();
+            var rates = webClient.DownloadString("https://api.exchangeratesapi.io/latest?base="+code);
+            ExchangeModel exchangeData = JsonConvert.DeserializeObject<ExchangeModel>(rates);
+            ViewBag.exchangeData = exchangeData;
+            ViewBag.code = code;
+
+            var currencies = webClient.DownloadString("https://openexchangerates.org/api/currencies.json");
+            CurrencyModel currencyData = JsonConvert.DeserializeObject<CurrencyModel>(currencies);
+            ViewBag.currencyData = currencyData;
+            return PartialView("Partials/View");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ CurrencyWebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CurrencyWebApplication.MvcApplication" Language="C#" %>
